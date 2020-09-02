0ce23f2d1065d73aa8989d808f1d9969542c9557
Caleb Chan <20009800@woodhouse.ac.uk>
2020-09-02 15:46:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle
+
+flower=turtle.Turtle()
+
+for i in range(999):
+  flower.forward(100)
+  flower.right(45)
+  flower.forward(100)
+  flower.right(135)
+  flower.forward(100)
+  flower.right(45)
+  flower.forward(100)
+  flower.right(135)
+  flower.right(36)
+
+ 
+
+
+turtle.done()
